5c37366708c9831fff4fa6bf61950595dba04314
LiaHar <lia-ht@yandex.ru>
2018-09-08 12:30:30 +0400

    sf

--- js1.js
+++ js1.js
@@ -1,3 +1,11 @@
 const h = 128;
 const b = 345;
-console.log(h * b + h);
+const f = h * b + h;
+const d = "hii";
+if(f === d || h === b) {
+    console.log(f + d);
+}
+if(f !== b && h !== d) {
+    const c = h / 2
+    console.log(c);
+}
